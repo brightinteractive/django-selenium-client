2bf2365d3f313b009c3c776033bb5dde256a9ab1
Tom Christie <tom@tomchristie.com>
2011-07-26 11:48:40 +0100

    Use selenium on plumpton by default.

--- djangoseleniumclient/testcase.py
+++ djangoseleniumclient/testcase.py
@@ -1,14 +1,25 @@
 from django.test import TestCase
 from testserver import TestServerThread
 from selenium import selenium
+import socket
 
+
+TEST_SERVER_HOSTNAME = '0.0.0.0'   # Network-visible, unlike 127.0.0.1
+TEST_SERVER_PORT = 8001
+
+SELENIUM_HOSTNAME = 'plumpton' 
+SELENIUM_PORT = 4444
+
+# See: http://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+LOCAL_IP_ADDR = socket.gethostbyname(socket.gethostname())
+TEST_SERVER_URL = 'http://%s:%d' % (LOCAL_IP_ADDR, TEST_SERVER_PORT)
 
 class UITestCase(TestCase):
     """
     TestCase that adds support for starting and stopping a live test server.
     """
 
-    def start_test_server(self, address='localhost', port=8001): 
+    def start_test_server(self, address=TEST_SERVER_HOSTNAME, port=TEST_SERVER_PORT): 
         """
         Creates a live test server object (instance of WSGIServer).
         """ 
@@ -32,9 +43,9 @@ class SeleniumTestCase(UITestCase):
         """
         Start a test server and tell selenium where to find it.
         """
-        self.start_test_server('localhost', 8001)
-        self.selenium = selenium('localhost', 4444,
-                                 '*pifirefox', 'http://localhost:8001') 
+        self.start_test_server(TEST_SERVER_HOSTNAME, TEST_SERVER_PORT)
+        self.selenium = selenium(SELENIUM_HOSTNAME, SELENIUM_PORT,
+                                 '*pifirefox', TEST_SERVER_URL) 
 
     def tearDown(self):
         """
