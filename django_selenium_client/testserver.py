bd80da350e354c19e1024252ba709dd540594b05
Tom Christie <tom@tomchristie.com>
2011-08-30 16:05:16 +0100

    Fixup selenium.

--- django_selenium_client/testserver.py
+++ django_selenium_client/testserver.py
@@ -54,7 +54,6 @@ class TestServerThread(threading.Thread):
             httpd = StoppableWSGIServer(server_address,
                                         basehttp.WSGIRequestHandler)
             httpd.set_app(handler)
-            self.started.set()
         except basehttp.WSGIServerException, e:
             self.error = e
             self.started.set()
@@ -74,6 +73,8 @@ class TestServerThread(threading.Thread):
                 # We have to use this slightly awkward syntax due to the fact
                 # that we're using *args and **kwargs together.
                 call_command('loaddata', *self.fixtures, **{'verbosity': 0})
+
+        self.started.set()
 
         # Loop until we get a stop event.
         while not self._stopevent.isSet():
